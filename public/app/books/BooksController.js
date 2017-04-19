afb886ecc94b79afefc1dccf19929c69d95ef1cb
Jashanpreet Singh <jashan.pahwa007@gmail.com>
2017-04-19 01:50:05 -0400

    my work around

--- public/app/books/BooksController.js
+++ public/app/books/BooksController.js
@@ -4,9 +4,12 @@
         .controller('BooksController', BooksController);
 
 
-    function BooksController() {
+    function BooksController(books, dataService, logger) {
 
         var vm = this;
+        vm.appName = books.appName;
+        vm.allBooks = dataService.getAllBooks();
+        logger.output('BooksController has been created');
 
     }
 
